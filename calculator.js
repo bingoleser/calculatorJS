be1eebfc9398a461a28a8e8313b4450cee746cb5
eser bingol <bingoleser21@gmail.com>
2023-06-15 08:52:02 +0300

    adding features to Calc

--- calculator.js
+++ calculator.js
@@ -1,2 +1,38 @@
 
+let runningTotal=0; //last total at background
+let screenNumber="0";
+let previousOperation=""
 
+
+function handleNumber(numberString) {
+    if(screenNumber==="0"){
+        screenNumber=numberString
+    }else{
+        screenNumber+=numberString
+    }
+    console.log(screenNumber)
+}
+
+
+function handleSymbol(params) {
+    console.log("symbol")
+    
+}
+
+
+
+function buttonClick(value){
+if (isNaN(value)) {
+    handleSymbol(value)
+}else
+  handleNumber(value)
+
+}
+
+function init() {
+    document.querySelector(".calc-buttons").addEventListener("click",(event)=>{
+        buttonClick(event.target.innerText)
+    })
+}
+
+init()
