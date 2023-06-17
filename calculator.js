2c0b33aa9b2f3fda7603e1c5c67bd198d69e64ad
eser bingol <bingoleser21@gmail.com>
2023-06-17 06:46:04 +0300

    app finished

--- calculator.js
+++ calculator.js
@@ -1,32 +1,84 @@
 
 let runningTotal=0; //last total at background
 let screenNumber="0";
-let previousOperation=""
-
+let previousOperation=null
+const screen= document.querySelector(".screen")
 
 function handleNumber(numberString) {
     if(screenNumber==="0"){
-        screenNumber=numberString
+        //if screen=0 means ,it is empty.add new number to screen
+       screenNumber=numberString
     }else{
-        screenNumber+=numberString
+        screenNumber+=numberString //already screen has numbers concate numbers
     }
-    console.log(screenNumber)
+    screen.innerText=screenNumber
 }
 
-
-function handleSymbol(params) {
-    console.log("symbol")
+function handleMath(params) {
     
+    if (screenNumber==="0"){
+        return;
+    }
+    const intScreenNumber=parseInt(screenNumber)
+
+    if (runningTotal=== 0) {
+        runningTotal=intScreenNumber
+    }else{
+        // already we have  number and going to work with them
+        flushOperation(intScreenNumber)
+    }
+    previousOperation=params
+    screenNumber="0"
 }
+function flushOperation(intNumber) {
+    console.log(intNumber)
+    if (previousOperation==="+") {
+        runningTotal+=intNumber
+    } else if (previousOperation==="−") {
+        runningTotal-=intNumber
+    } else if (previousOperation==="×") {
+        runningTotal*=intNumber
+    } else if (previousOperation==="÷") {
+        runningTotal =runningTotal/intNumber
+    }
+}
+function handleSymbol(params) {
+   
+    switch (params) {
+        case "C":
+            screenNumber="0";
+            runningTotal=0
+        break;
+        case '=':
+            if (previousOperation=== null) {
+                return
+            }
+            flushOperation(parseInt(screenNumber));
+            previousOperation=null;
+            screenNumber=runningTotal;
+            //runningTotal=0
+            break;
+        case "+":
+        case "−":
+        case "÷":
+         case "×":
+          handleMath(params)
+         break;
+    }
+ 
+}
+
+
 
 
 
 function buttonClick(value){
 if (isNaN(value)) {
     handleSymbol(value)
-}else
+}else{
   handleNumber(value)
-
+    }
+screen.innerText=screenNumber
 }
 
 function init() {
